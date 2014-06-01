21431f788b52953587e92560ee7bfc894775e476
templer <templer@192.168.1.106>
2014-06-01 13:13:50 +0300

    Delete IEmployee

--- src/company/Company.java
+++ src/company/Company.java
@@ -1,5 +1,6 @@
 package company;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Company implements ICompany{
 	
@@ -20,6 +21,18 @@ public class Company implements ICompany{
 
 	public void setEmployees(ArrayList<Employee> employees) {
 		this.employees = employees;
+	}
+
+	@Override
+	public void find(Comparator<Employee> c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sort(Comparator<Employee> c) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
